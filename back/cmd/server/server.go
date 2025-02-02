b5883281760499803e647933d4a9174be1653376
finkabaj <yuriy9505@gmail.com>
2025-02-02 13:08:03 +0500

    fix: allow any

--- back/cmd/server/server.go
+++ back/cmd/server/server.go
@@ -57,7 +57,7 @@ func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
-	r.Use(myMiddleware.Cors([]string{"http://localhost:3000"}))
+	r.Use(myMiddleware.Cors([]string{"*"}))
 
 	wsServer := websocket.NewServer()
 
