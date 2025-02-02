224669e9215f2691d2b05e80ca48a46f83fa9110
finkabaj <yuriy9505@gmail.com>
2025-02-02 14:16:38 +0500

    fix: allow any

--- back/internal/middleware/cors.go
+++ back/internal/middleware/cors.go
@@ -15,15 +15,6 @@ func Cors(allowedOrigins []string) func(http.Handler) http.Handler {
 				}
 			}
 
-			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, PATCH")
-			w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, Authorization")
-			w.Header().Set("Access-Control-Allow-Credentials", "true")
-
-			if r.Method == "OPTIONS" {
-				w.WriteHeader(http.StatusOK)
-				return
-			}
-
 			next.ServeHTTP(w, r)
 		})
 	}
